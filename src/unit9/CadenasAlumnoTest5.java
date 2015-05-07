ea7a9bbca7ad5575f2691f1312ac7e58f516fb7a
Luz <luzonil@hotmail.com>
2015-05-07 16:26:15 +0200

    añado CadenasAlumnoTest5 modificado

--- src/unit9/CadenasAlumnoTest5.java
+++ src/unit9/CadenasAlumnoTest5.java
@@ -14,6 +14,7 @@ public class CadenasAlumnoTest5 {
 		assertTrue(cd1.reemplazaTodos("jamonmonmon", "mon", "bon") == "jamonmonmon".replaceAll("mon", "bon"));
 	}
 
+	@Test
 	public void testReemplazaTodos2() {
 		System.out.println("Comparando dos cadenas...");
 		CadenasAlumno cd1 = new CadenasAlumno();
