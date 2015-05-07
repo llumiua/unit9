bf4480a8745ebad4e48565c8abb932901b8223d3
Luz <luzonil@hotmail.com>
2015-05-07 16:03:24 +0200

    añado CadenasAlumnoTest3

---
+++ src/unit9/CadenasAlumnoTest3.java
@@ -0,0 +1,41 @@
+package unit9;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CadenasAlumnoTest3 {
+
+	@Test
+	public void testEsMenor1() {
+		System.out.println("Comparando dos cadenas...");
+		CadenasAlumno cd1 = new CadenasAlumno();
+		
+		assertTrue(cd1.esMenor("entornos", "ento") == "entornos".compareTo("ento") < 0);
+	}
+	
+	@Test
+	public void testEsMenor2() {
+		System.out.println("Comparando dos cadenas...");
+		CadenasAlumno cd1 = new CadenasAlumno();
+		
+		assertTrue(cd1.esMenor("ento","entornos") == "ento".compareTo("entornos") < 0);
+	}
+	
+	@Test
+	public void testEsMenor3() {
+		System.out.println("Comparando dos cadenas...");
+		CadenasAlumno cd1 = new CadenasAlumno();
+		
+		assertTrue(cd1.esMenor("entornos","sonrotne") == "entornos".compareTo("sonrotne") < 0);
+	}
+
+	@Test
+	public void testEsMenor4() {
+		System.out.println("Comparando dos cadenas...");
+		CadenasAlumno cd1 = new CadenasAlumno();
+		
+		assertTrue(cd1.esMenor("entornos","entornos") == "entornos".compareTo("entornos") < 0);
+	}
+	
+}
