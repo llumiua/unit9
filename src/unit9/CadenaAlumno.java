9725fcabb311ef0506dbd1c3249f05408ed86aa8
Luz <luzonil@hotmail.com>
2015-05-07 10:54:02 +0200

    añado CadenaAlumno.java

---
+++ src/unit9/CadenaAlumno.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package unit9;
+
+/**
+ * @author Luz Feliciano Sanjuan
+ *
+ */
+public class CadenaAlumno {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
